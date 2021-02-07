d08653a6478b9fe5faf984ab8d40f1cfc862de24
Boris Cirok <boris.cirok@tipsport.cz>
2021-02-07 21:54:09 +0100

    refactoring

---
+++ src/logic/gameLogic.ts
@@ -0,0 +1,73 @@
+export type GameRow = boolean[]
+
+export type Game = GameRow[]
+
+/**
+ *
+ * 123 game[row-1][column-1] | game[row-1][column] | game[row-1][column+1]
+ * 4X5 game[row][column-1] | X | game[row][column+1]
+ * 678 game[row+1][column-1] | game[row+1][column] | game[row+1][column+1]
+ */
+export function getAliveNeighboursCount(row: number, column: number, game: Game) {
+  let aliveNeighboursCount = 0
+  const isFirstColumn = column === 0
+  const isLastColumn = column === game[row].length - 1
+  const isFirstRow = row === 0
+  const isLastRow = row === game.length - 1
+
+  if (!isFirstRow) {
+    // 1
+    if (!isFirstColumn && game[row - 1][column - 1]) {
+      aliveNeighboursCount++
+    }
+    // 2
+    if (game[row - 1][column]) {
+      aliveNeighboursCount++
+    }
+    // 3
+    if (!isLastColumn && game[row - 1][column + 1]) {
+      aliveNeighboursCount++
+    }
+  }
+  // 4
+  if (!isFirstColumn && game[row][column - 1]) {
+    aliveNeighboursCount++
+  }
+  // 5
+  if (!isLastColumn && game[row][column + 1]) {
+    aliveNeighboursCount++
+  }
+
+  if (!isLastRow) {
+    // 6
+    if (!isFirstColumn && game[row + 1][column - 1]) {
+      aliveNeighboursCount++
+    }
+    // 7
+    if (game[row + 1][column]) {
+      aliveNeighboursCount++
+    }
+
+    // 8
+    if (!isLastColumn && game[row + 1][column + 1]) {
+      aliveNeighboursCount++
+    }
+  }
+
+  return aliveNeighboursCount
+}
+
+export function isCellActive(row: number, column: number, game: Game) {
+  const neighboursCount = getAliveNeighboursCount(row, column, game)
+  const isNowAlive = game[row][column]
+  if (isNowAlive && neighboursCount < 2) {
+    return false
+  } else if (isNowAlive && (neighboursCount === 2 || neighboursCount === 3)) {
+    return true
+  } else if (isNowAlive && neighboursCount >= 3) {
+    return false
+  } else if (!isNowAlive && neighboursCount === 3) {
+    return true
+  }
+  return false
+}
--- src/redux/reducers/appReducer.ts
+++ src/redux/reducers/appReducer.ts
@@ -1,9 +1,6 @@
 import { range } from 'lodash'
 import { actionsKeys, AppActions } from '../actions/appActionsTypes'
-
-type GameRow = boolean[]
-
-type Game = GameRow[]
+import { Game, isCellActive } from '../../logic/gameLogic'
 
 export type State = {
   game: Game
@@ -13,88 +10,22 @@ export type State = {
 
 export const initialState: State = {
   game: [],
-  columns: 1,
-  rows: 1
+  columns: 0,
+  rows: 0
 }
 
-/**
- *
- * 123 game[row-1][column-1] | game[row-1][column] | game[row-1][column+1]
- * 4X5 game[row][column-1] | X | game[row][column+1]
- * 678 game[row+1][column-1] | game[row+1][column] | game[row+1][column+1]
- */
-function getAliveNeighboursCount(row: number, column: number, game: Game) {
-  let aliveNeighboursCount = 0
-  const isFirstColumn = column === 0
-  const isLastColumn = column === game[row].length - 1
-  const isFirstRow = row === 0
-  const isLastRow = row === game.length - 1
-
-  if (!isFirstRow) {
-    // 1
-    if (!isFirstColumn && game[row - 1][column - 1]) {
-      aliveNeighboursCount++
-    }
-    // 2
-    if (game[row - 1][column]) {
-      aliveNeighboursCount++
-    }
-    // 3
-    if (!isLastColumn && game[row - 1][column + 1]) {
-      aliveNeighboursCount++
-    }
-  }
-  // 4
-  if (!isFirstColumn && game[row][column - 1]) {
-    aliveNeighboursCount++
-  }
-  // 5
-  if (!isLastColumn && game[row][column + 1]) {
-    aliveNeighboursCount++
-  }
-
-  if (!isLastRow) {
-    // 6
-    if (!isFirstColumn && game[row + 1][column - 1]) {
-      aliveNeighboursCount++
-    }
-    // 7
-    if (game[row + 1][column]) {
-      aliveNeighboursCount++
-    }
-
-    // 8
-    if (!isLastColumn && game[row + 1][column + 1]) {
-      aliveNeighboursCount++
-    }
-  }
-
-  return aliveNeighboursCount
-}
-
-function isCellActive(row: number, column: number, game: Game) {
-  const neighboursCount = getAliveNeighboursCount(row, column, game)
-  const isNowAlive = game[row][column]
-  if (isNowAlive && neighboursCount < 2) {
-    return false
-  } else if (isNowAlive && (neighboursCount === 2 || neighboursCount === 3)) {
-    return true
-  } else if (isNowAlive && neighboursCount >= 3) {
-    return false
-  } else if (!isNowAlive && neighboursCount === 3) {
-    return true
-  }
-  return false
+function getNewGame(rows: number, columns: number): Game {
+  return range(rows).map(() => {
+    return range(columns).map(() => false)
+  })
 }
 
 export function appReducer(state: State = initialState, action: AppActions) {
   switch (action.type) {
-    case actionsKeys.INIT_GAME:
+    case actionsKeys.INIT_GAME: {
       const { rows, columns } = action.payload
       return {
-        game: range(rows).map(() => {
-          return range(columns).map(() => false)
-        }),
+        game: getNewGame(rows, columns),
         columns,
         rows
       }
